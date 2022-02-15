ee82d0d21120d9e63434443b0a05490c73dcac71
AR <aki1013@naver.com>
2022-02-15 10:48:35 +0900

    소켓통신 단방향 2

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,8 +1,11 @@
 package site.metacoding.chat;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyClientSocket {
 
@@ -14,8 +17,17 @@ public class MyClientSocket {
             socket = new Socket("localhost", 1078);
             writer = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("안녕\n"); // \n은 메세지의 끝을 알려주는것
-            writer.flush(); // 버퍼에 담은후 물 내려주는것
+
+            // 스캐너 달고 반복 (x)
+            Scanner sc = new Scanner(System.in);
+            // 키보드로 입력받는 부분
+            while (true) {
+                String inputData = sc.nextLine();
+                writer.write(inputData + "\n"); // \n은 메세지의 끝을 알려주는것
+                writer.flush(); // 버퍼에 담은후 물 내려주는것
+
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,15 +1,22 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
     ServerSocket serverSocket; // 리스너 (연결 =세션)
     Socket socket; // 메시지 통신
     BufferedReader reader;
+
+    // 추가(클라이언트에게 메세지 보내기)
+    BufferedWriter writer;
+    Scanner sc;
 
     public MyServerSocket() {
         try {
@@ -20,9 +27,17 @@ public class MyServerSocket {
             socket = serverSocket.accept(); // while을 돌면서 대기(랜덤포트) - 클라이언트가 접속할 때까지
             reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
-            String inputData = reader.readLine();
-            System.out.println("받은메세지: " + inputData);
-            System.out.println("클라이언트 연결됨");
+
+            // 메시지 보내기
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            sc = new Scanner(System.in);
+            // 메시지 반복해서 받는 서버 소켓
+            while (true) {
+                String inputData = reader.readLine();
+                System.out.println("받은메세지: " + inputData);
+
+            }
+
         } catch (Exception e) {
             System.out.println("통신오류발생" + e.getMessage());
             // e.printStackTrace(); //어느파일에서 잘못됐는지 확인하려면.!
